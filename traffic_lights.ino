151ee408c8531f10fbfb20839f75cdc48401caa2
büşra <74591664+busraeskiyurt@users.noreply.github.com>
2021-06-11 21:14:27 +0300

    Add files via upload

---
+++ traffic_lights.ino
@@ -0,0 +1,23 @@
+void setup() {
+  pinMode (8, OUTPUT);     
+  pinMode (9, OUTPUT);
+  pinMode (10, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(8, HIGH);   //We burning pin 8. We turn off the others.
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  delay(3000);              //Here we wait for 3 seconds.
+
+  digitalWrite(8, LOW);    //We burning pin 9. We turn off the others.
+  digitalWrite(9, HIGH);
+  digitalWrite(10, LOW);
+  delay(1000);              //Here we wait for 1 seconds.
+
+  digitalWrite(8, LOW);     //We burning pin 10. We turn off the others.
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+  delay(3000);              //Here we wait for 3 seconds.Here we wait for 3 seconds.
+
+}
