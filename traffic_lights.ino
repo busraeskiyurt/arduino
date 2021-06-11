fee1fb75e9293bb181240abdc45dec6ed4681801
büşra <74591664+busraeskiyurt@users.noreply.github.com>
2021-06-11 21:17:03 +0300

    Update traffic_lights.ino

--- traffic_lights.ino
+++ traffic_lights.ino
@@ -5,19 +5,19 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(8, HIGH);   //We burning pin 8. We turn off the others.
+  digitalWrite(8, HIGH);   //We burning led 8. We turn off the others.
   digitalWrite(9, LOW);
   digitalWrite(10, LOW);
   delay(3000);              //Here we wait for 3 seconds.
 
-  digitalWrite(8, LOW);    //We burning pin 9. We turn off the others.
+  digitalWrite(8, LOW);    //We burning led 9. We turn off the others.
   digitalWrite(9, HIGH);
   digitalWrite(10, LOW);
   delay(1000);              //Here we wait for 1 seconds.
 
-  digitalWrite(8, LOW);     //We burning pin 10. We turn off the others.
+  digitalWrite(8, LOW);     //We burning led 10. We turn off the others.
   digitalWrite(9, LOW);
   digitalWrite(10, HIGH);
-  delay(3000);              //Here we wait for 3 seconds.Here we wait for 3 seconds.
+  delay(3000);              //Here we wait for 3 seconds.
 
 }
